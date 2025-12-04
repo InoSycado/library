d33578e75b04ae92419d20723eba56d6fd5068cc
Inooooo <rizalino35@gmail.com>
2025-12-04 13:15:00 +0800

    ExtensionAdded
    Redirect Folder

---
+++ addons_library/Library_Extension/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Library Extensions",
+    "version": "1.0.0",
+    "depends": [
+        "base",
+        "library"
+    ],
+    "external_dependencies": {},
+    "author": "Rizalino I. Caguioa III",
+    "summary": """A module extended from Library""",
+    "description": """
+        A module extended from Library
+    """,
+    "category": "Extra Tools",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/library_book_views.xml",
+        "views/library_book_category_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
